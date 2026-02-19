fe1f4cd1eb11107ba48a12e274bc53ba10e64b44
Victor Kiptoo Kimitei <kimiteivictor@gmail.com>
2026-02-19 10:40:16 +0300

    Rendering Function JS

--- script.js
+++ script.js
@@ -28,3 +28,39 @@ function Place(name, county, landmarks, date, desc, rating) {
 // INITIALIZING THE SYSTEM
 const myPlaces = new PlaceManager();
 
+// RENDERING THE PLACES 
+function renderPlaces() {
+    var container = document.getElementById('cards-container');
+    container.innerHTML = ""; // clear previous content
+
+    var placesArr = Object.values(myPlaces.places);
+
+    if (placesArr.length === 0) {
+        container.innerHTML = "<p>No places visited yet!</p>";
+        return;
+    }
+
+    placesArr.forEach(function(place) {
+        var card = document.createElement('div');
+        card.className = 'placeCard';
+        card.style.position = 'relative';
+        card.style.height = '250px';
+
+        card.innerHTML = ''
+            + '<div class="cardFront">'
+            + '<h3>' + place.name + '</h3>'
+            + '<p>⭐ ' + place.rating + '/5</p>'
+            + '</div>'
+            + '<div class="cardBack">'
+            + '<h4>' + place.name + '</h4>'
+            + '<p><strong>County:</strong> ' + place.county + '</p>'
+            + '<p><strong>Landmarks:</strong> ' + place.landmarks + '</p>'
+            + '<p><strong>Date:</strong> ' + place.date + '</p>'
+            + '<p>' + place.desc + '</p>'
+            + '<button onclick="handleDelete(' + place.id + ')">Delete</button>'
+            + '</div>';
+
+        container.appendChild(card);
+    });
+}
+
